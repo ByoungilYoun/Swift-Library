eab4189476f250e145ea635625cc5b8cc8af4d71
YounByoungil <jimmy7525@naver.com>
2020-12-08 00:54:57 +0900

    Alamofire Request Practice

--- AlamofirePractice(NaverPapago)/AlamofirePractice(NaverPapago)/ViewController.swift
+++ AlamofirePractice(NaverPapago)/AlamofirePractice(NaverPapago)/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireObjectMapper
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    var url = "https://openapi.naver.com/v1/papago/n2mt"
+    var params = ["source":"ko",
+                  "target":"en",
+                  "text":"만나서 반갑습니다."]
+    var header = ["Content-Type":"application/x-www-form-urlencoded; charset=UTF-8",
+                  "X-Naver-Client-Id":"s1rLaCZwXZr0ucfihipC",
+                  "X-Naver-Client-Secret":"UBztJN1c8F"]
+    
+    Alamofire.request(url, method: .post, parameters: params, encoding: URLEncoding.default, headers: header).responseJSON { (response) in
+      print(response.result.value)
+    }
   }
 
 
