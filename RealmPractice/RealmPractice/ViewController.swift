9ba376fe48a0aad39ab7623b65399976604a05c8
YounByoungil <jimmy7525@naver.com>
2020-12-12 01:15:34 +0900

    Setup Project

---
+++ RealmPractice/RealmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by 윤병일 on 2020/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
