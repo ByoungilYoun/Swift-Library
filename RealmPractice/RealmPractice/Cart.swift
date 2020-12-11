615744d834d7cab37a88ff97cede1e20fe52b5eb
YounByoungil <jimmy7525@naver.com>
2020-12-12 02:20:25 +0900

    Adjust Realm

---
+++ RealmPractice/RealmPractice/Cart.swift
@@ -0,0 +1,14 @@
+//
+//  Cart.swift
+//  RealmPractice
+//
+//  Created by 윤병일 on 2020/12/12.
+//
+
+import UIKit
+import RealmSwift
+
+
+class Cart : Object {
+  @objc dynamic var name = ""
+}
--- RealmPractice/RealmPractice/ViewController.swift
+++ RealmPractice/RealmPractice/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
@@ -28,10 +29,33 @@ class ViewController: UIViewController {
   
   private let tableView = UITableView()
   
+  var notificationToken : NotificationToken?
+  var realm : Realm?
+  var items : Results<Cart>?
+  
   //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
+    
+    // Migration 이 필요할때, 데이터베이스 초기화
+    let config = Realm.Configuration( deleteRealmIfMigrationNeeded: true)
+    
+    Realm.Configuration.defaultConfiguration = config
+    
+    realm = try! Realm()
+    
+    // Cart 데이터들을 수집하는 코드
+    items = realm?.objects(Cart.self) // Cart 로 구성된 database 가 items 에 들어가게 된다.
+    
+    // Push Driven 작동 시키는 코드 (database 에 변경사항이 있을때 reload 를 한다.)
+    notificationToken = realm?.observe({ (noti, realm) in
+      self.tableView.reloadData()
+    })
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    notificationToken?.invalidate()
     
   }
   
@@ -72,7 +96,11 @@ class ViewController: UIViewController {
   }
   
   @objc func btnClick() {
-    print("button clicked..")
+    let cart = Cart()
+    cart.name = textField.text!
+    try! realm?.write {
+      realm?.add(cart)
+    }
   }
 
 }
@@ -80,12 +108,12 @@ class ViewController: UIViewController {
 //MARK: - UITableViewDataSource
 extension ViewController : UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return (items?.count)!
   }
-  
+  g
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = "hi"
+    cell.textLabel?.text = items![indexPath.row].name
     return cell
   }
   
