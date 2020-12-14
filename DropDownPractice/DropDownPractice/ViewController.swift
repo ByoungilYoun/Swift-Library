bdaf0acb2bbcaad4fbe057e1c7436c02860f62f1
YounByoungil <jimmy7525@naver.com>
2020-12-15 00:33:57 +0900

    Make a Drop Down Button

--- DropDownPractice/DropDownPractice/ViewController.swift
+++ DropDownPractice/DropDownPractice/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  private let button : UIButton = {
+    let bt = UIButton()
+    bt.setTitle("Drop Down", for: .normal)
+    bt.setTitleColor(.black, for: .normal)
+    bt.backgroundColor = .yellow
+    return bt
+  }()
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    configureUI()
   }
 
 
+  //MARK: - configureUI()
+  private func configureUI() {
+    view.addSubview(button)
+    
+    button.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
+  }
 }
 
