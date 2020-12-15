96a85c9c89a56e6e5621870d978bc3b365a3ec08
YounByoungil <jimmy7525@naver.com>
2020-12-15 23:16:20 +0900

    Setup Project

---
+++ Toast-Practice/Toast-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toast-Practice
+//
+//  Created by 윤병일 on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
