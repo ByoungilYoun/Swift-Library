ddccfeb8a2583b99ef30e5d9451b2193bf119b08
YounByoungil <jimmy7525@naver.com>
2020-12-16 00:39:44 +0900

    Toast Practice

--- Toast-Practice/Toast-Practice/ViewController.swift
+++ Toast-Practice/Toast-Practice/ViewController.swift
@@ -1,4 +1,4 @@
-o//
+//
 //  ViewController.swift
 //  Toast-Practice
 //
@@ -6,14 +6,28 @@ o//
 //
 
 import UIKit
+import Toast_Swift
 
 class ViewController: UIViewController {
 
+  //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .white
+    // 기본 토스트 메세지 날리기
+//    self.view.makeToast("토스트 메세지 입니다.")
+    
+    // 이미지를 이용해서도 사용할 수 있다.
+    self.view.makeToast("This is a piece of toast", duration: 5.0, position : .bottom, title: "Toast Title", image: #imageLiteral(resourceName: "Ben")) { didTap in
+      if didTap {
+        // 탭을 했을때
+        print("completion from tap")
+      } else {
+        // 탭을 하지 않았을때
+        print("completion without tap")
+      }
+    }
   }
 
 
 }
-
