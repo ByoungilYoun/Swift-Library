e310560d41c2bd90f7ffa0729ab78b076a07c55b
YounByoungil <jimmy7525@naver.com>
2020-12-19 01:40:14 +0900

    Make a button

--- IndicatorViewPractice/IndicatorViewPractice/ViewController.swift
+++ IndicatorViewPractice/IndicatorViewPractice/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  private let button : UIButton = {
+    let bt = UIButton()
+    bt.setTitle("button", for: .normal)
+    bt.setTitleColor(.blue, for: .normal)
+    bt.backgroundColor = .yellow
+    bt.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    return bt
+  }()
+  
+  //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    configureUI()
   }
 
+  //MARK: - configureUI()
+  private func configureUI() {
+    view.addSubview(button)
+    
+    button.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+      button.widthAnchor.constraint(equalToConstant: 80),
+      button.heightAnchor.constraint(equalToConstant: 50)
+    ])
+  }
 
+  //MARK: - @objc func
+  @objc func buttonTapped() {
+    print("123")
+  }
 }
 
