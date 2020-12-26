f4b88421ce313535942249554e89ea55e8633b25
YounByoungil <jimmy7525@naver.com>
2020-12-27 01:17:26 +0900

    SkeletionView view에 적용

--- SkeletionViewPractice/SkeletionViewPractice/ViewController.swift
+++ SkeletionViewPractice/SkeletionViewPractice/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import SkeletonView
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  lazy var myView : UIView = {
+    let v = UIView()
+    v.backgroundColor = .lightGray
+    return v
+  }()
+  
+  //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = .white
+    self.view.addSubview(myView)
+    
+    self.myView.translatesAutoresizingMaskIntoConstraints = false
+    self.myView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    self.myView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+    self.myView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    self.myView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    
+    // skeletionview 적용
+    self.myView.isSkeletonable = true
+    self.myView.showAnimatedGradientSkeleton()
+    
   }
 
 
