2d542003309aeb3103d93c010709b77d3c50dcc1
YounByoungil <jimmy7525@naver.com>
2020-12-27 01:30:32 +0900

    SkeletionView label 에 적용

--- SkeletionViewPractice/SkeletionViewPractice/ViewController.swift
+++ SkeletionViewPractice/SkeletionViewPractice/ViewController.swift
@@ -11,30 +11,36 @@ import SkeletonView
 class ViewController: UIViewController {
 
   //MARK: - Properties
-  lazy var myView : UIView = {
-    let v = UIView()
-    v.backgroundColor = .lightGray
-    return v
+  lazy var myLabel : UILabel = {
+    let l = UILabel()
+    l.backgroundColor = .lightGray
+    l.textColor = .white
+    l.textColor = .black
+    l.text = "Hello, welcome to my App"
+    l.textAlignment = .center
+    return l
   }()
   
   //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = .white
-    self.view.addSubview(myView)
+    self.view.addSubview(myLabel)
     
-    self.myView.translatesAutoresizingMaskIntoConstraints = false
-    self.myView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-    self.myView.widthAnchor.constraint(equalToConstant: 100).isActive = true
-    self.myView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-    self.myView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    self.myLabel.translatesAutoresizingMaskIntoConstraints = false
+    self.myLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    self.myLabel.widthAnchor.constraint(equalToConstant: 320).isActive = true
+    self.myLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    self.myLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
     
-    // skeletionview 적용
-    self.myView.isSkeletonable = true
-    self.myView.showAnimatedGradientSkeleton()
+    // skeletionView 적용
+    self.myLabel.isSkeletonable = true
+    self.myLabel.showAnimatedGradientSkeleton()
     
+    // skeletionView 사라지게 하기
+    DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+      self.myLabel.hideSkeleton()
+    }
   }
-
-
 }
 
